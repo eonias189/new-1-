b6f48637273fe349048947cb68abfe0924eac834
eonias <eonias189@yandex.ru>
2022-11-21 23:37:29 +0400

    commit 3

--- release/main.py
+++ release/main.py
@@ -1,21 +1,28 @@
 import sys
 import sqlite3
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTableWidgetItem, QMessageBox
 
+from addEditCoffeeForm import Ui_Form
+from mainUi import Ui_MainWindow
 
-class AddEditForm(QWidget):
+
+class AddEditForm(QWidget, Ui_Form):
+    def __init__(self, mw):
+        super().__init__()
+        self.mw = mw
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        if self.mw:
+            self.mw.is_creating = False
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('addEditCoffeeForm.ui', self)
-
-
-class MyWidget(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('main.ui', self)
-        self.con = sqlite3.connect('coffee.sqlite')
+        self.setupUi(self)
+        self.con = sqlite3.connect('data/coffee.sqlite')
         self.is_creating = False
         self.load_table()
         self.add.clicked.connect(self.add_value)
@@ -40,7 +47,6 @@ vkus, price, obyom FROM data""").fetchall()
         self.is_creating = True
         cur = self.con.cursor()
         new_id = max([i[0] for i in cur.execute('SELECT ID FROM data').fetchall()]) + 1
-        cur.execute(f"INSERT INTO data VALUES ({new_id}, '', '', 0, '', 0, 0)")
         self.cur_id = new_id
         self.redact_value()
 
@@ -50,7 +56,7 @@ vkus, price, obyom FROM data""").fetchall()
             cur = self.con.cursor()
             sort, step_obj, type_c, vkus, price, obyom = cur.execute(f"""SELECT sort, step_obj,
 (SELECT value FROM Type WHERE id=type_id), vkus, price, obyom FROM data WHERE ID={self.cur_id}""").fetchall()[0]
-        self.widget = AddEditForm()
+        self.widget = AddEditForm(self)
         if not self.is_creating:
             self.widget.sort.setText(sort)
             self.widget.step_obj.setText(step_obj)
@@ -58,7 +64,6 @@ vkus, price, obyom FROM data""").fetchall()
             self.widget.vkus.setText(vkus)
             self.widget.price.setText(str(price))
             self.widget.obyom.setText(str(obyom))
-        self.is_creating = False
         self.widget.show()
         self.widget.ok.clicked.connect(self.redact_value_1)
 
@@ -67,13 +72,15 @@ vkus, price, obyom FROM data""").fetchall()
                                                      self.widget.type.currentText(), self.widget.vkus.text(), \
                                                      self.widget.price.text(), self.widget.obyom.text()
         if not all([sort, step_obj, vkus, price, obyom]):
-            message = QMessageBox.about(self.widget, 'Ошибка', 'не должно быть пустых строк для ввода')
+            ms = QMessageBox.about(self.widget, 'Ошибка', 'не должно быть пустых строк для ввода')
         elif not price.isdigit():
-            message = QMessageBox.about(self.widget, 'Ошибка', 'цена должна быть целым положительным числом')
+            ms = QMessageBox.about(self.widget, 'Ошибка', 'цена должна быть целым положительным числом')
         elif not obyom.isdigit():
-            message = QMessageBox.about(self.widget, 'Ошибка', 'объём должен быть целым положительным числом')
+            ms = QMessageBox.about(self.widget, 'Ошибка', 'объём должен быть целым положительным числом')
         else:
             cur = self.con.cursor()
+            if self.is_creating:
+                cur.execute(f"INSERT INTO data VALUES ({self.cur_id}, '', '', 0, '', 0, 0)")
             cur.execute(f"""UPDATE data SET
 sort='{sort}', step_obj='{step_obj}', type_id=(SELECT id FROM Type WHERE value='{type_c}'),
 vkus='{vkus}', price={price}, obyom={obyom} WHERE ID={self.cur_id}""")
